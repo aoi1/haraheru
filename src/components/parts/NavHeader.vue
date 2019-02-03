c17a2adb9323d7e878d987df5322ea459d6a68aa
blux2 <tkhsaoi88@gmail.com>
2019-02-03 19:32:12 +0900

    modified menu bar for smartphone

--- src/components/parts/NavHeader.vue
+++ src/components/parts/NavHeader.vue
@@ -1,15 +1,9 @@
 <template>
   <nav class="header-nav">
-    <v-toolbar>
-    <v-toolbar-title>haraheru.com</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat to="/">Home</v-btn>
-      <v-btn flat to="/news">News</v-btn>
-      <v-btn flat to="/about">About</v-btn>
-      <v-btn flat to="/works">Works</v-btn>
-      <v-btn flat to="/links">Links</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+      <v-btn flat small to="/">Home</v-btn>
+      <v-btn flat small to="/news">News</v-btn>
+      <v-btn flat small to="/about">About</v-btn>
+      <v-btn flat small to="/works">Works</v-btn>
+      <v-btn flat small to="/links">Links</v-btn>
   </nav>
 </template>
